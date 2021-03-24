807d67acc2a7d7afe2c084893cec00069153d9be
Klimchuk Nikita <KlimchukNik@gmail.com>
2021-03-24 18:34:49 +0300

    Y Complementary Filter added

---
+++ Y Complementary Filter/Y Complementary Filter.ino
@@ -0,0 +1,71 @@
+/*
+  Project Motion_Tracking_System
+  Copyright (c) 2021 Electronic Device Development Club
+  https://github.com/KlimchukNikita
+
+  Development: <EDDC> 2021
+  Example Demo: Y Complementary Filter
+*/
+
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+#define TO_DEG 57.29577951308232087679815481410517033f
+#define T_OUT 20
+#define P_OUT 50
+#define FK 0.1
+
+MPU6050 accelgyro;
+
+float angle_ay, angle_gy, angle_cpl;
+int dt = 0;
+long int t_next, p_next;
+
+float clamp(float v, float minv, float maxv)
+{
+    if (v > maxv)
+        return maxv;
+
+    else if(v < minv)
+        return minv;
+
+    return v;
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    accelgyro.initialize();
+}
+
+void loop()
+{
+    long int t = millis();
+
+    if (t_next < t)
+    {
+        int16_t ax_raw, ay_raw, az_raw, gx_raw, gy_raw, gz_raw;
+        float ax, gy;
+
+        t_next = t + T_OUT;
+
+        accelgyro.getMotion6(&ax_raw, &ay_raw, &az_raw, &gx_raw, &gy_raw, &gz_raw);
+
+        gy = gy_raw / 16.4;
+        ax = ax_raw / 4096.0;
+        ax = clamp(ax, -1.0, 1.0);
+
+        angle_ay = 90 - TO_DEG * acos(ax);
+        angle_gy = angle_gy + gy * T_OUT / 1000.0;
+        angle_gy = angle_gy * (1 - FK) - angle_ay * FK;
+    }
+
+    t = millis();
+
+    if (p_next < t)
+    {
+        p_next = t + P_OUT;
+        Serial.println(angle_gy);
+    }
+}
